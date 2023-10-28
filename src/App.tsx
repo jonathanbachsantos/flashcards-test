ddca9ae99b2c5bb9d4a5ea4071012fc38035a990
Jonathan Bach <jo.bach.s@gmail.com>
2023-10-28 10:43:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppStyles.css";
+
+import Cards from "./Cards";
+
+export default function App() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+}
